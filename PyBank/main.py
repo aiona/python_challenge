cf111f9b53ff24b7a3379ccaa01c31a290fb68e9
Adam Iona <adamiona@Adams-MacBook-Pro.local>
2019-12-14 23:52:24 -0500

    Pushed final changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,13 +5,10 @@ import csv
 globalReader = []
 #Specify file path
 csvpath = os.path.join("budget_data.csv")
+output_file = "budget_analysis.txt"
 
 with open(csvpath, newline= "") as csvfile:
 	#Read header row
-	#csvheader = ['Date','Profit/Losses']
-	
-	#print(f"CSV Header: {csvheader}")
-	
 	csvreader = csv.reader(csvfile, delimiter=',')
 	print(csvreader)
 	csvheader = next(csvreader)
@@ -37,8 +34,7 @@ total_profit = 0
 previous_profit = 867884
 previous_change = 0
 greatest_increase_month = ""
-#greatest_increase_month = largest value in profit_changes
-#greatest_decrease_month = smallest value in profit_changes
+greatest_decrease_month = ""
 profit_change = 0
 profit_changes = []
 months = []
@@ -52,45 +48,46 @@ for row in reader:
 			
 	#Calculate profit change
 	profit_change = int(row[1]) - int(previous_profit)
-	print(profit_change)
+	#print(profit_change)
 	profit_changes.append(profit_change)
 	months.append(row[0])
 	
 	previous_profit = row[1]
-	#min_month = 
-	#max_month = 
 	
-	#sorting a list in python 
-	#
-	
-	#compare current change with previous
-	#Help!
+	for pc in profit_changes:
+		if profit_change == max(profit_changes):
+			greatest_increase_month = row[0]
+		if profit_change == min(profit_changes):
+			greatest_decrease_month = row[0]
 
-for change in profit_changes:
-	if profit_change > previous_change:
-		greatest_increase_month=(row[0])			
-	
-	previous_profit = int(row[1])
-	
-	#Calculate profit change month profit[1]- profit[0]
-	
+
+if max(profit_changes) == int(row[1]):
+	print(greatest_increase_month)	
 	
 #Calculate average change 
 average_change = sum(profit_changes)/(len(profit_changes)-1)
-
-#Keep track of months
-#index of min month, index max month	
-	
-#compare greatest increase to profit change
-#maximum of profit_changes
-#if profit_change[n] > profit_change[n-1]
-	#then greatest_profit_month
-
-#Calculate greatest decrease in profits
-#compare greatest_decrease to profit_change
  	
 print("Total: " +str(total_profit))
 print("Average Change: $" + f"{average_change:.2f}")
-print("Greatest Increase in Profits: " + greatest_increase_month +  "($" + str(max(profit_changes)) + ")")
-print("Greatest Decrease in Profits: " + "($" + str(min(profit_changes)) + ")")
+print("Greatest Increase in Profits: " + greatest_increase_month +  " ($" + str(max(profit_changes)) + ")")
+print("Greatest Decrease in Profits: " + greatest_decrease_month + " ($" + str(min(profit_changes)) + ")")
+
+#Output files
+
+with open(output_file, "w") as textfile:
+	textfile.write("Financial Analysis")
+	textfile.write("\n")
+	textfile.write("-------------------")
+	textfile.write("\n")
+	textfile.write("Total Months: " + str(len(months)))
+	textfile.write("\n")
+	textfile.write("Total: " + str(total_profit))
+	textfile.write("\n")
+	textfile.write("Average Change: $" + f"{average_change:.2f}")
+	textfile.write("\n")
+	textfile.write("Greatest Increase in Profits: " + greatest_increase_month +  " ($" + str(max(profit_changes)) + ")")
+	textfile.write("\n")
+	textfile.write("Greatest Decrease in Profits: " + greatest_decrease_month + " ($" + str(min(profit_changes)) + ")")
+	
+
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,12 +5,10 @@ import csv
 globalReader = []
 #Specify file path
 csvpath = os.path.join("election_data.csv")
+output_file = "election_results.txt"
 
 with open(csvpath, newline= "") as csvfile:
 	#Read header row
-	#csvheader = ['Date','Profit/Losses']
-	#print(f"CSV Header: {csvheader}")
-	
 	csvreader = csv.reader(csvfile, delimiter=',')
 	print(csvreader)
 	csvheader = next(csvreader)
@@ -30,7 +28,8 @@ correy_votes = 0
 li_votes = 0
 li_percent = 0
 otooley_votes = 0
-otooley_percent = 0		
+otooley_percent = 0
+most_votes = 0	
 		
 #Loop through rows
 for row in reader:
@@ -48,25 +47,57 @@ for row in reader:
 		otooley_votes +=1 		
 		
 		
-khan_percent = khan_votes//vote_count
-correy_percent = correy_votes//vote_count
-li_percent = li_votes//vote_count
-otooley_percent = otooley_votes//vote_count
+khan_percent = round((khan_votes/(len(votes) - 1) * 100), 3)
+correy_percent = round((correy_votes/(len(votes) - 1) * 100), 3)
+li_percent = round((li_votes/(len(votes) - 1) * 100), 3)
+otooley_percent = round((otooley_votes/(len(votes) - 1) * 100), 3)
 
+#Calculate winner
+candidate_totals = [khan_votes, correy_votes, li_votes, otooley_votes]
+winner = ""
+
+for total in candidate_totals:
+	if khan_votes == max(candidate_totals):
+		winner = "Khan"
+	elif correy_votes == max(candidate_totals):
+		winner == "Correy"
+	elif li_votes == max(candidate_totals):
+		winner = "Li"
+	elif otooley_votes == max(candidate_totals):
+		winner = "O'Tooley"
+	
 		
 
+print("")
 print("Election Results")
 print("-----------------------")
-
 print("Total Votes: " + str(len(votes)- 1))
 print("-----------------------")
-print("Khan:" + str(khan_percent) + "%" + " (" + str(khan_votes) + ")")
-print("Correy:" + str(correy_votes))
-print("Li:" + str(li_votes))
-print("O'Tooley:" + str(correy_votes))
+print("Khan: " + f"{khan_percent:3f}" + "%" + " (" + str(khan_votes) + ")")
+print("Correy: " + f"{correy_percent:.3f}" + "%" + " (" + str(correy_votes) + ")")
+print("Li: " + f"{li_percent:.3f}" + "%" + " (" + str(li_votes) + ")")
+print("O'Tooley: " + f"{otooley_percent:.3f}" + "%" + " (" + str(otooley_votes) + ")")
 print("-----------------------")
-print("-----------------------")
-print("Winner: ")
+print("Winner: " + winner)
 print("-----------------------")
 
+
+with open(output_file, "w") as textfile:
+	textfile.write("Election Results")
+	textfile.write("\n")
+	textfile.write("-----------------------")
+	textfile.write("\n")
+	textfile.write("Khan: " + f"{khan_percent:3f}" + "%" + " (" + str(khan_votes) + ")")
+	textfile.write("\n")
+	textfile.write("Correy: " + f"{correy_percent:.3f}" + "%" + " (" + str(correy_votes) + ")")
+	textfile.write("\n")
+	textfile.write("Li: " + f"{li_percent:.3f}" + "%" + " (" + str(li_votes) + ")")
+	textfile.write("\n")
+	textfile.write("O'Tooley: " + f"{otooley_percent:.3f}" + "%" + " (" + str(otooley_votes) + ")")
+	textfile.write("\n")
+	textfile.write("-----------------------")
+	textfile.write("\n")
+	textfile.write("Winner: " + winner)
+	textfile.write("\n")
+	textfile.write("-----------------------")
 
